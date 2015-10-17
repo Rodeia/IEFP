36db375b7bdfa7394e7d7e9948bb963f584d0482
marcojoao <marcojoao11@gmail.com>
2015-10-17 02:44:44 +0100

    Bug fix's

--- Aula01/Aula01/Program.cs
+++ Aula01/Aula01/Program.cs
@@ -10,15 +10,17 @@ namespace Aula01
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.Clear();
             Random aleatorio = new Random();
-            long com = aleatorio.Next(1, 1000);
-            long pontos = 1000;
-            long tentativas = 10;
+            int com = aleatorio.Next(1, 1000);
+            Console.WriteLine(com);
+            int pontos = 1000;
+            int tentativas = 10;
             long maxima = 1000;
             long minima = 1;
             long user = 0;
             bool aux = false;
             Console.WriteLine("O computador escolheu um numero aleatório entre {0} e {1}, qual acha que é?", minima, maxima);
             Console.WriteLine();
+            Console.Write("#Aposta> ");
         Start:
             if (tentativas > 0)
             {
@@ -28,51 +30,69 @@ namespace Aula01
                     {
                         if (aux == true)
                         {
-                            Console.WriteLine("Numero invalido! insere um numero entre 1 e 1000");
-                            Console.WriteLine("");
+                            Console.WriteLine("Numero invalido! Tem de ser um numero entre 1 e 1000");
+                            Console.WriteLine();
+                            Console.Write("#Aposta> ");
                         }
                     String userin = Console.ReadLine();
                     try
                     {
-                        user = long.Parse(userin);
+                            user = long.Parse(userin); 
                     }
                     catch (FormatException)
                     {
                         Console.WriteLine("Isso não é um numero!");
                         Console.WriteLine();
+                        aux = false;
+                        Console.Write("#Aposta> ");
                         goto Start;
                     }
                         aux = true;
-                    } while (user <0 || user > 1000);
+                    } while (user < 0 || user > 1000);
                     aux = false;
-                    if (user == com)
+                    if (user == com && tentativas == 10)
                     {
-                        if (tentativas == 10)
-                        {
                             Console.WriteLine("Ganhaste logo há primeira!!!");
                             Console.WriteLine();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ganhaste Com {0} pontos e {1} tentativas restantes!", pontos, tentativas);
-                            Console.WriteLine();
-                        }
+                            Console.Write("Carrega ENTER para terminar o jogo.");
+                            Console.ReadKey();
+                    }
+                    else if (user == com)
+                    {
+                        Console.WriteLine("Ganhaste Com {0} pontos e {1} tentativas restantes!", pontos, tentativas);
+                        Console.WriteLine();
+                        Console.Write("Carrega ENTER para terminar o jogo.");
+                        Console.ReadKey();
                     }
                     else
                     {
                         if (user > com)
                         {
+                            tentativas = tentativas - 1;
+                            pontos = pontos - 100;
                             if (user < maxima)
                             {
                                 maxima = user;
                             }
-                            tentativas = tentativas - 1;
-                            pontos = pontos - 100;
-                            if (tentativas >= 1)
+                            if (tentativas > 0)
                             {
-                                Console.WriteLine("O numero procurado é inferior!Tens {0} e {1} tentativas restantes.", pontos, tentativas);
-                                Console.WriteLine("O numero que o computador escolheu está entre os números {0} e {1}", minima, maxima);
-                                Console.WriteLine();
+                                if (user > maxima)
+                                {
+                                    Console.WriteLine("ATENÇÃO: O numero inserido é maior que a máxima {0}!", maxima);
+                                    Console.WriteLine("Procura entre {0} e {1}", minima, maxima);
+                                    Console.WriteLine();
+                                    tentativas = tentativas + 1;
+                                    pontos = pontos + 100;
+                                    Console.Write("#Aposta> ");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("O numero procurado é inferior!");
+                                    Console.WriteLine("Tens {0} pontos e {1} tentativas restantes.", pontos, tentativas);
+                                    Console.WriteLine("Procura entre {0} e {1}", minima, maxima);
+                                    Console.WriteLine();
+                                    Console.Write("#Aposta> ");
+                                }
                             }
                             else
                             {
@@ -82,26 +102,42 @@ namespace Aula01
                         }
                         else
                         {
+                            tentativas = tentativas - 1;
+                            pontos = pontos - 100;
                             if (user > minima)
                             {
                                 minima = user;
                             }
-                            tentativas = tentativas - 1;
-                            pontos = pontos - 100;
-                            if (tentativas >= 1)
+                            if (tentativas > 0)
                             {
-                                Console.WriteLine("O numero procurado é maior!!!Tens {0} e {1} tentativas restantes.", pontos, tentativas);
-                                Console.WriteLine("O numero que o computador escolheu está entre os números {0} e {1}", minima, maxima);
-                                Console.WriteLine();
+                                if (user < minima)
+                                {
+                                    Console.WriteLine("ATENÇÃO: O numero inserido é menor que a mínima {0}!", minima);
+                                    Console.WriteLine("Procura entre {0} e {1}", minima, maxima);
+                                    Console.WriteLine();
+                                    tentativas = tentativas + 1;
+                                    pontos = pontos + 100;
+                                    Console.Write("#Aposta> ");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("O numero procurado é maior!");
+                                    Console.WriteLine("Tens {0} pontos e {1} tentativas restantes.", pontos, tentativas);
+                                    Console.WriteLine("Procura entre {0} e {1}", minima, maxima);
+                                    Console.WriteLine();
+                                    Console.Write("#Aposta> ");
+                                }
                             }
                             else
                             {
                                 Console.WriteLine("Perdeste , o numero certo era {0}", com);
                                 Console.WriteLine();
+                                Console.Write("Carrega ENTER para terminar o jogo.");
+                                Console.ReadKey();
                             }
                         }
                     }
-                } while (tentativas > 0 || user != com);
+                } while (user != com || tentativas < 0);
             }
         }
     }
